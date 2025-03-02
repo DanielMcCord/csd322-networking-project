e2812bc277b56ec41c59699b3ea324ce1ecb3135
Daniel McCord <daniel.mccord@lwtech.edu>
2025-03-02 13:14:29 -0800

    Complete milestone 1

--- messages.py
+++ messages.py
@@ -1,39 +1,72 @@
 """
-Application layer messages for the Shakespear word-frequency distributed app.
+Application layer messages for the Shakespeare word-frequency distributed app.
 Defines all relevant message types for the application.
 For each message type defines its serialization into a human-readable ASCII
 format that can be sent over the network.
 Also defines the deserialization when receiving each message type from the
 network.
 """
-
+from functools import cache
 from io import *
+from typing import Final
+from abc import abstractmethod
 
-class Message():
+ENDL: Final[str] = "\r\n"
+"""
+Line terminator.
+"""
+
+
+class Message:
     """
     Base class for every app message type.
     """
 
+    @staticmethod
+    @cache
+    def prefixes() -> dict:
+        """
+        Returns a mapping of serialized message prefix strings to their corresponding classes.
+        """
+
+        return {
+            "GET ": HTTPGetRequest,
+            "ReqW": GetWorkRequest,
+            "AsgW": GetWorkResponse,
+            "SubW": WorkCompleteRequest,
+            "AckW": WorkCompleteResponse
+        }
+
+    @abstractmethod
     def serialize(self) -> str:
         """
         Returns a string corresponding to the human-readable ASCII message that
         can be sent over the network.
-        The default base implementation intentionally raises an exception to
-        ensure that each concrete message type implements this method.
         """
 
-        raise NotImplementedError
-
-    def deserialize(msg: TextIOBase):
+    @classmethod
+    def deserialize(cls, msg: TextIOBase):
         """
         Parses the specified msg and returns its deserialized Message object (or
-        raises a ValueException if the msg is not a valid serialization).
+        raises a ValueError if the msg is not a valid serialization).
         """
 
         firstline = msg.readline()
-        if firstline.startswith("GET "):
-            return HTTPGetRequest.deserialize(firstline, msg)
-        raise ValueError
+
+        for prefix, subclass in cls.prefixes().items():
+            if firstline.startswith(prefix):
+                return subclass.parse(firstline, msg)
+        raise ValueError(f"No recognized prefix found")
+
+    @staticmethod
+    @abstractmethod
+    def parse(firstline: str, rest: TextIOBase):
+        """
+        Parses the specified firstline and rest (of message) as an instance of a
+        specific subclass of Message. Returns the resulting Message object, or
+        raises a ValueError on parsing failure.
+        """
+
 
 class HTTPGetRequest(Message):
     """
@@ -51,48 +84,52 @@ class HTTPGetRequest(Message):
         self.host = host
         self.path = path
 
-    # Overrides super().serialize()
+    # Override
     def serialize(self) -> str:
-        result = "GET %s HTTP/1.1\r\n" % (self.path)
-        result += "Host: %s\r\n" % (self.host)
-        result += "\r\n"
-        return result 
-    
-    def deserialize(firstline: str, rest: TextIOBase) -> Message:
+        return (f"GET {self.path} HTTP/1.1{ENDL}"
+                f"Host: {self.host}{ENDL}"
+                f"{ENDL}")
+
+    @staticmethod
+    def parse(firstline: str, rest: TextIOBase) -> Message:
         """
         Parses the specified firstline and rest (of message) and returns an
-        HTTPGetRequest (or raises a ValueException if the msg is not a valid
+        HTTPGetRequest (or raises a ValueError if the msg is not a valid
         serialized HTTPGetRequest).
-        This is a class method i.e. associated with the class rather than
-        instances of the class. 
         """
 
-        if firstline.startswith("GET "):
-            [_, path, _] = firstline.split()
-            line = rest.readline()
-            [name, value] = line.split()
-            if name == "Host:":
-                return HTTPGetRequest(value, path)
-        raise ValueError
+        if not firstline.startswith("GET "): raise ValueError("Bad prefix")
+        split = firstline.split()
+        if len(split) != 3: raise ValueError("Incorrect number of elements")
+        [_, path, _] = split
+        split = rest.readline().split()
+        if len(split) != 2: raise ValueError("Incorrect number of elements")
+        [name, value] = split
+        if not name == "Host:": raise ValueError("Unexpected field")
+        return HTTPGetRequest(value, path)
+
 
 class GetWorkRequest(Message):
     """
     Message sent by a Volunteer to the Coordinator to ask for work.
     """
 
+    # Override
     def serialize(self) -> str:
-        return "REQWORK"
-    
-    def deserialize(firstline: str, rest: TextIOBase) -> Message:
+        return (f"ReqW{ENDL}"
+                f"{ENDL}")
+
+    @staticmethod
+    def parse(firstline: str, rest: TextIOBase) -> Message:
         """
         Parses the specified firstline and rest (of message) and returns a
-        GetWorkRequest (or raises a ValueException if the msg is not a valid
+        GetWorkRequest (or raises a ValueError if the msg is not a valid
         serialized GetWorkRequest).
         """
 
-        # TODO: implement this.
-        if msg != "REQWORK": raise ValueError
+        if not firstline.startswith("ReqW"): raise ValueError("Bad prefix")
         return GetWorkRequest()
+
 
 class GetWorkResponse(Message):
     """
@@ -110,19 +147,33 @@ class GetWorkResponse(Message):
         self.host = host
         self.path = path
 
+    # Override
+    def serialize(self) -> str:
+        return (f"AsgW{ENDL}"
+                f"Host: {self.host}{ENDL}"
+                f"Path: {self.path}{ENDL}"
+                f"{ENDL}")
 
-    # TODO: override serialize().
-    
-    def deserialize(firstline: str, rest: TextIOBase) -> Message:
+    @staticmethod
+    def parse(firstline: str, rest: TextIOBase) -> Message:
         """
         Parses the specified firstline and rest (of message) and returns a
-        GetWorkResponse (or raises a ValueException if the msg is not a valid
+        GetWorkResponse (or raises a ValueError if the msg is not a valid
         serialized GetWorkResponse).
         """
 
-        # TODO: implement this.
-        raise NotImplementedError
-    
+        if not firstline.startswith("AsgW"): raise ValueError("Bad prefix")
+
+        args = []
+        for expected_name in ("Host:", "Path:"):
+            split = rest.readline().split()
+            if not split: raise ValueError("Incorrect number of elements")
+            name, value = split[0], split[1] if len(split) > 1 else ""
+            if name != expected_name: raise ValueError("Unexpected field")
+            args.append(value)
+        return GetWorkResponse(*args)
+
+
 class WorkCompleteRequest(Message):
     """
     Message sent by a Volunteer to the Coordinator to inform the Coordinator
@@ -134,38 +185,65 @@ class WorkCompleteRequest(Message):
         """
         Constructs a WorkCompleteRequest reporting the word-frequency result
         (dictionary with word as key and count as value) for the specified
-        Shakespeare play (indentified by the path).
+        Shakespeare play (identified by the path).
         """
-        
+
         self.path = path
         self.word_counts = word_counts
 
-    # TODO: override serialize().
-    
-    def deserialize(firstline: str, rest: TextIOBase) -> Message:
+    # Override
+    def serialize(self) -> str:
+        return (f"SubW{ENDL}"
+                f"Path: {self.path}{ENDL}"
+                f"Word-Counts:{ENDL}" +
+                "".join(f"{word} {count}{ENDL}" for word, count in self.word_counts.items()) +
+                f"{ENDL}")
+
+    @staticmethod
+    def parse(firstline: str, rest: TextIOBase) -> Message:
         """
         Parses the specified firstline and rest (of message) and returns a
-        WorkCompleteRequest (or raises a ValueException if the msg is not a
+        WorkCompleteRequest (or raises a ValueError if the msg is not a
         valid serialized WorkCompleteRequest).
         """
 
-        # TODO: implement this.
-        raise NotImplementedError
-    
+        if not firstline.startswith("SubW"): raise ValueError("Bad prefix")
+
+        split = rest.readline().split()
+        if len(split) != 2: raise ValueError("Incorrect number of elements")
+        name, path = split[0], split[1]
+        if not name == "Path:": raise ValueError("Unexpected field")
+
+        split = rest.readline().split()
+        if len(split) != 1: raise ValueError("Incorrect number of elements")
+        name = split[0]
+        if name != "Word-Counts:": raise ValueError("Unexpected field")
+
+        word_counts = {}
+        while len(split := rest.readline().split()) > 0:
+            if len(split) != 2: raise ValueError("Incorrect number of elements")
+            word_counts[split[0]] = int(split[1])
+        return WorkCompleteRequest(path, word_counts)
+
+
 class WorkCompleteResponse(Message):
     """
     Message sent by the Coordinator to a Volunteer in response to a
     WorkCompleteRequest.
     """
 
-    # TODO: override serialize().
-    
-    def deserialize(firstline: str, rest: TextIOBase) -> Message:
+    # Override
+    def serialize(self) -> str:
+        return (f"AckW{ENDL}"
+                f"{ENDL}")
+
+    @staticmethod
+    def parse(firstline: str, rest: TextIOBase) -> Message:
         """
         Parses the specified firstline and rest (of message) and returns a
-        WorkCompleteResponse (or raises a ValueException if the msg is not a
+        WorkCompleteResponse (or raises a ValueError if the msg is not a
         valid serialized WorkCompleteResponse).
         """
-        
-        # TODO: implement this.
-        raise NotImplementedError
+
+        if not firstline.startswith("AckW"): raise ValueError("Bad prefix")
+        return WorkCompleteResponse()
--- messages_test.py
+++ messages_test.py
@@ -1,17 +1,19 @@
 """Tests for messages.py."""
 
-from io import *
-from messages import *
 import unittest
+
+from messages import *
+
 
 class TestMessage(unittest.TestCase):
 
     def test_invalid_deserialize(self):
         try:
-           deserialized = Message.deserialize(StringIO("bogus msg"))
-           self.fail("Expected ValueError") 
-        except(ValueError):
+            Message.deserialize(StringIO("bogus msg"))
+            self.fail("Expected ValueError")
+        except ValueError:
             pass
+
 
 class TestHTTPGetRequest(unittest.TestCase):
 
@@ -25,9 +27,9 @@ class TestHTTPGetRequest(unittest.TestCase):
         request = HTTPGetRequest("some-host", "some-path")
 
         serialized = request.serialize()
-        expected_serialized = "GET some-path HTTP/1.1\r\n"
-        expected_serialized += "Host: some-host\r\n"
-        expected_serialized += "\r\n"
+        expected_serialized = ("GET some-path HTTP/1.1\r\n"
+                               "Host: some-host\r\n"
+                               "\r\n")
         self.assertEqual(serialized, expected_serialized)
 
         deserialized = Message.deserialize(StringIO(serialized))
@@ -35,18 +37,20 @@ class TestHTTPGetRequest(unittest.TestCase):
         self.assertEqual(deserialized.host, "some-host")
         self.assertEqual(deserialized.path, "some-path")
 
+
 class TestGetWorkRequest(unittest.TestCase):
 
     def test_serialize_deserialize(self):
         request = GetWorkRequest()
 
         serialized = request.serialize()
-        if serialized != "REQWORK":
-            self.fail()
+        expected_serialized = ("ReqW\r\n"
+                               "\r\n")
+        self.assertEqual(serialized, expected_serialized)
 
-        deserialized = GetWorkRequest.deserialize(StringIO(serialized))
+        deserialized = Message.deserialize(StringIO(serialized))
         self.assertIsInstance(deserialized, GetWorkRequest)
-        # TODO: validate
+
 
 class TestGetWorkResponse(unittest.TestCase):
 
@@ -54,13 +58,32 @@ class TestGetWorkResponse(unittest.TestCase):
         request = GetWorkResponse("some-host", "some-path")
 
         serialized = request.serialize()
-        # TODO: validate
+        expected_serialized = ("AsgW\r\n"
+                               "Host: some-host\r\n"
+                               "Path: some-path\r\n"
+                               "\r\n")
+        self.assertEqual(serialized, expected_serialized)
 
-        deserialized = GetWorkResponse.deserialize(StringIO(serialized))
+        deserialized = Message.deserialize(StringIO(serialized))
         self.assertIsInstance(deserialized, GetWorkResponse)
-        # TODO: validate
+        self.assertEqual(deserialized.host, "some-host")
+        self.assertEqual(deserialized.path, "some-path")
 
-    # TODO: also validate the case of no work left
+    def test_serialize_deserialize_no_work_left(self):
+        request = GetWorkResponse("", "")
+
+        serialized = request.serialize()
+        expected_serialized = ("AsgW\r\n"
+                               "Host: \r\n"
+                               "Path: \r\n"
+                               "\r\n")
+        self.assertEqual(serialized, expected_serialized)
+
+        deserialized = Message.deserialize(StringIO(serialized))
+        self.assertIsInstance(deserialized, GetWorkResponse)
+        self.assertEqual(deserialized.host, "")
+        self.assertEqual(deserialized.path, "")
+
 
 class TestWorkCompleteRequest(unittest.TestCase):
 
@@ -69,11 +92,20 @@ class TestWorkCompleteRequest(unittest.TestCase):
                                       {"romeo": 16, "juliet": 18, "rose": 7})
 
         serialized = request.serialize()
-        # TODO: validate
+        expected_serialized = ("SubW\r\n"
+                               "Path: path-to-play\r\n"
+                               "Word-Counts:\r\n"
+                               "romeo 16\r\n"
+                               "juliet 18\r\n"
+                               "rose 7\r\n"
+                               "\r\n")
+        self.assertEqual(serialized, expected_serialized)
 
-        deserialized = WorkCompleteRequest.deserialize(StringIO(serialized))
+        deserialized = Message.deserialize(StringIO(serialized))
         self.assertIsInstance(deserialized, WorkCompleteRequest)
-        # TODO: validate
+        self.assertEqual(deserialized.path, "path-to-play")
+        self.assertEqual(deserialized.word_counts, {"romeo": 16, "juliet": 18, "rose": 7})
+
 
 class TestWorkCompleteResponse(unittest.TestCase):
 
@@ -81,11 +113,13 @@ class TestWorkCompleteResponse(unittest.TestCase):
         request = WorkCompleteResponse()
 
         serialized = request.serialize()
-        # TODO: validate
+        expected_serialized = ("AckW\r\n"
+                               "\r\n")
+        self.assertEqual(serialized, expected_serialized)
 
-        deserialized = WorkCompleteResponse.deserialize(StringIO(serialized))
+        deserialized = Message.deserialize(StringIO(serialized))
         self.assertIsInstance(deserialized, WorkCompleteResponse)
-        # TODO: validate
+
 
 if __name__ == '__main__':
     unittest.main()
